567b91fac824bdada192138f05fd5ff49a0c6baa
Justice Addison <jay@tisqo.com>
2013-06-28 20:22:15 +0100

    setting up app structure and adding a slugifier

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,16 @@ define(['angular', 'services'], function (angular) {
 
 	return angular.module('budgetApp.controllers', ['budgetApp.services'])
 		// Sample controller where service is being used
-		.controller('MyCtrl1', ['$scope', 'version', function ($scope, version) {
-			$scope.scopedAppVersion = version;
+		.controller('ctlBudgetList', ['$scope', function ($scope) {
+			require(['controllers/ctlBudgetList'], function(ctlBudgetList) {
+
+				// injector method takes an array of modules as the first argument
+				// if you want your controller to be able to use components from
+				// any of your other modules, make sure you include it together with 'ng'
+				// Furthermore we need to pass on the $scope as it's unique to this controller
+				angular.injector(['ng']).invoke(ctlBudgetList, this, {'$scope': $scope});
+
+			});
 		}])
 		// More involved example where controller is required from an external file
 		.controller('MyCtrl2', ['$scope', function($scope) {
--- app/js/routes.js
+++ app/js/routes.js
@@ -2,15 +2,18 @@ define(['angular', 'app'], function(angular, app) {
 	'use strict';
 
 	return app.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/view1', {
-			templateUrl: '/app/partials/partial1.html',
-			controller: 'MyCtrl1'
-		});
-		$routeProvider.when('/view2', {
-			templateUrl: '/app/partials/partial2.html',
-			controller: 'MyCtrl2'
-		});
-		$routeProvider.otherwise({redirectTo: '/view1'});
+		
+		$routeProvider.
+			when('/budgets', {
+				templateUrl: '/app/partials/budget-list.html',
+				controller: 'ctlBudgetList'
+			}).
+			when('/budgets/:id', {
+				templateUrl: '/app/partials/budget.html',
+				controller: 'ctrl-budget'
+			});
+
+		$routeProvider.otherwise({redirectTo: '/budgets'});
 	}]);
 
 });
